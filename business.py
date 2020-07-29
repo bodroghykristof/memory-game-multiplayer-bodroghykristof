cdfd6a74460a4103d489bad3eedf2d60b8651b68
bodroghykristof <bodroghykristof@gmail.com>
2020-07-29 09:18:05 +0200

    Create business and data_handler layers

---
+++ business.py
@@ -0,0 +1,6 @@
+def register_user():
+    pass
+
+
+def check_login_data():
+    pass
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO
+import business
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = b'n\x18\xd9Pi\x98NB\xa7`i\xf5\xfb#\xa1\x1e'
 socketio = SocketIO(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main_page():
+    if request.method == 'POST':
+        business.check_login_data()
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        business.register_user()
     return render_template('register.html')
 
 
