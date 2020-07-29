120eca2d2758d129cbf6ff057c82347d76e01a37
bodroghykristof <bodroghykristof@gmail.com>
2020-07-29 09:12:26 +0200

    Create registration page

--- main.py
+++ main.py
@@ -11,5 +11,10 @@ def main_page():
     return render_template('login.html')
 
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
     socketio.run(app)
